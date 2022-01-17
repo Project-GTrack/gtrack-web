d482de6cdb08506de27aed10c922e33bf9308bd4
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-01-17 20:44:31 +0800

    Update employees

--- client/src/components/employees/InactiveAccountsComponent.js
+++ client/src/components/employees/InactiveAccountsComponent.js
@@ -5,7 +5,7 @@ const InactiveAccountsComponent = () => {
     const columns = ["Last Name", "First Name", "Email", "Contact Number","Address","Age","Gender","Date Added","Status"];
 
     const data = [
-    ["Wayne", "Bruce", "batman@gmail.com", "09123456789","America","52","Male","01/02/22","Active"],
+    ["Wayne", "Bruce", "batman@gmail.com", "09123456789","America","52","Male","01/02/22","Inactive"],
     ];
 
     const options = {

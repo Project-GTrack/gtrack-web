fc91033f368536744239af326913e7ae4ee735f7
Cloyd Vincent Anis <cloyd.anis@gmail.com>
2022-01-23 16:50:53 +0800

    Updated modals in dashboard card

--- client/src/components/dashboard/DashboardCard.js
+++ client/src/components/dashboard/DashboardCard.js
@@ -1,33 +1,19 @@
 import React from "react";
-import { Card,CardHeader,CardContent,Typography, Box,Modal } from "@mui/material"; 
+import { Card,CardHeader,CardContent,Typography} from "@mui/material"; 
 import DriversModal from "./modals/DriversModal";
 import TrucksModal from "./modals/TrucksModal";
 import CollectionsModal from "./modals/CollectionsModal";
 import DumpsterModal from "./modals/DumpstersModal";
 function DashboardCard(dashcard){
-    const [openDriver, setOpenDriver] = React.useState(false);
-    const handleOpenDriver = () => setOpenDriver(true);
-    const handleCloseDriver = () => setOpenDriver(false);
-    
-    const [openTruck, setOpenTruck] = React.useState(false);
-    const handleOpenTruck = () => setOpenTruck(true);
-    const handleCloseTruck = () => setOpenTruck(false);
-
-    const [openDumpster, setOpenDumpster] = React.useState(false);
-    const handleOpenDumpster = () => setOpenDumpster(true);
-    const handleCloseDumpster = () => setOpenDumpster(false);
-
-    const [openCollection, setOpenCollection] = React.useState(false);
-    const handleOpenCollection = () => setOpenCollection(true);
-    const handleCloseCollection = () => setOpenCollection(false);
-
-
-    console.log(dashcard)
+    const [openModal, setOpenModal] = React.useState(false);
+    const handleOpenModal = () => setOpenModal(true);
+    const handleCloseModal = () => setOpenModal(false);
+  
 
     return(
     
         <div>
-            <Card onClick={handleOpenDriver} elevation={1}>
+            <Card onClick={handleOpenModal} elevation={1}>
                 <CardHeader title={dashcard.title}/>
                 <CardContent>
                     <Typography  id={dashcard.id}  variant="h5" color="textSecondary">
@@ -38,27 +24,27 @@ function DashboardCard(dashcard){
             </Card>
             {dashcard.id === 1 ?  
             <DriversModal
-              openModal={openDriver}
-              handleCloseModal={handleCloseDriver}
-              handleOpenModal={handleOpenDriver}
+              openModal={openModal}
+              handleCloseModal={handleCloseModal}
+              handleOpenModal={handleOpenModal}
             /> : null}
             {dashcard.id === 2 ?  
             <TrucksModal
-              openModal={openTruck}
-              handleCloseModal={handleCloseTruck}
-              handleOpenModal={handleOpenTruck}
+              openModal={openModal}
+              handleCloseModal={handleCloseModal}
+              handleOpenModal={handleOpenModal}
             /> : null}
             {dashcard.id === 3 ?  
             <DumpsterModal
-              openModal={openDumpster}
-              handleCloseModal={handleCloseDumpster}
-              handleOpenModal={handleOpenDumpster}
+              openModal={openModal}
+              handleCloseModal={handleCloseModal}
+              handleOpenModal={handleOpenModal}
             /> : null}
             {dashcard.id === 4 ?  
             <CollectionsModal
-              openModal={openCollection}
-              handleCloseModal={handleCloseCollection}
-              handleOpenModal={handleOpenCollection}
+              openModal={openModal}
+              handleCloseModal={handleCloseModal}
+              handleOpenModal={handleOpenModal}
             /> : null}
 
            

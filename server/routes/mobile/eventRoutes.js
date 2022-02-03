19d599f4c915c873397e40ae89f274248b73281d
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-02-03 20:51:35 +0800

    Added event controller and routes, Updated migrations and model

--- server/routes/mobile/eventRoutes.js
+++ server/routes/mobile/eventRoutes.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const eventController=require("../../controllers/mobile/eventController");
 
 router.get("/get-events",eventController.getEvents);
+router.post("/join-event/:id",eventController.joinEvent);
 
 module.exports=router;

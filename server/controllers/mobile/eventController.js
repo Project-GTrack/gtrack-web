b9bd793ab2708292de802a12313017a3db30dd1f
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-02-08 18:07:14 +0800

    Update account controller

--- server/controllers/mobile/eventController.js
+++ server/controllers/mobile/eventController.js
@@ -3,33 +3,34 @@ require('dotenv').config("../../.env");
 var C = require("crypto-js");
 const event=require("../../models/event");
 const announcement=require("../../models/announcement");
-const admin=require("../../models/user");
 const user=require("../../models/user");
 const attLine=require("../../models/attachment_line");
 const attachment=require("../../models/attachment");
 const eventParticipant=require("../../models/event_participant");
 const report=require("../../models/report");
-const schedule=require("../../models/report");
-const truck=require("../../models/report");
-const truck_assignment=require("../../models/report");
+const schedule=require("../../models/schedule");
+const truck=require("../../models/truck");
+const truck_assignment=require("../../models/truck_assignment");
 
 
-admin.model.hasMany(event.model, {foreignKey: 'admin_id', as: 'adminEvent'});
-event.model.belongsTo(admin.model, {foreignKey: 'admin_id', as: 'eventAdmin'});
+user.model.hasMany(event.model, {foreignKey: 'admin_id', as: 'adminEvent'});
+event.model.belongsTo(user.model, {foreignKey: 'admin_id', as: 'eventAdmin'});
 event.model.hasOne(attLine.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id', as: 'eventLine'});
 attLine.model.hasOne(event.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id', as: 'lineEvent'});
-admin.model.hasMany(announcement.model, {foreignKey: 'admin_id', as: 'adminAnnouncement'});
-announcement.model.belongsTo(admin.model, {foreignKey: 'admin_id', as: 'announcementAdmin'});
+user.model.hasMany(announcement.model, {foreignKey: 'admin_id', as: 'adminAnnouncement'});
+announcement.model.belongsTo(user.model, {foreignKey: 'admin_id', as: 'announcementAdmin'});
 announcement.model.hasOne(attLine.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id', as: 'announcementLine'});
 attLine.model.hasOne(announcement.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id', as: 'lineAnnouncement'});
 attLine.model.hasMany(attachment.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id',as:'lineAttachment'});
 attachment.model.belongsTo(attLine.model, {foreignKey: 'attachment_line_id', sourceKey: 'attachment_line_id', as: 'attachementLine'});
 eventParticipant.model.hasOne(event.model, {foreignKey: 'event_id', as: 'partiEvent'});
 event.model.hasMany(eventParticipant.model, {foreignKey: 'event_id', as: 'eventParti'});
-eventParticipant.model.hasOne(admin.model, {foreignKey: 'user_id', as: 'partiUser'});
-admin.model.hasMany(eventParticipant.model, {foreignKey: 'user_id', as: 'userPari'});
+eventParticipant.model.hasOne(user.model, {foreignKey: 'user_id', as: 'partiUser'});
+user.model.hasMany(eventParticipant.model, {foreignKey: 'user_id', as: 'userPari'});
 report.model.hasOne(attLine.model, {foreignKey: 'attachment_line_id', as: 'reportLine'});
 attLine.model.hasOne(report.model, {foreignKey: 'attachment_line_id', as: 'lineReport'});
+// user.model.hasMany(schedule.model, {foreignKey: 'driver_id', targetKey:'user_id', as: 'userSchedule'});
+// schedule.model.hasOne(user.model, {foreignKey:'driver_id', targetKey:'user_id', as:'schedUser'});
 
 exports.getEvents=async (req,res)=>{
     let posts=await event.model.findAll({
@@ -37,7 +38,7 @@ exports.getEvents=async (req,res)=>{
             deletedAt:null
         },
         include:[{
-            model: admin.model, as:"eventAdmin"
+            model: user.model, as:"eventAdmin"
         },{
             model: attLine.model, as:"eventLine",
             include:[{
@@ -53,7 +54,7 @@ exports.getEvents=async (req,res)=>{
 }
 
 exports.joinEvent = async (req, res) => {
-    let account=await admin.model.findOne({ where: { email: req.body.email } });
+    let account=await user.model.findOne({ where: { email: req.body.email } });
     if(account){
         var decrypted = C.AES.decrypt(account.password,process.env.SECRET_KEY);
         if(req.body.password===decrypted.toString(C.enc.Utf8)){

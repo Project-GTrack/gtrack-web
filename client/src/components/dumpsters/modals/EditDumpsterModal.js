48a7a72d971c780c1b1d71615db4f264a151c02c
aljannondoy <54362999+aljannondoy@users.noreply.github.com>
2022-02-17 22:35:29 +0800

    Updated dumpster

--- client/src/components/dumpsters/modals/EditDumpsterModal.js
+++ client/src/components/dumpsters/modals/EditDumpsterModal.js
@@ -1,27 +1,18 @@
 import * as React from "react";
-import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
-import { useForm } from "react-hook-form";
 import DialogContent from "@mui/material/DialogContent";
 import axios from "axios";
-import GoogleMapReact from "google-map-react";
 import DialogActions from "@mui/material/DialogActions";
 import PropTypes from "prop-types";
 import Cookies from "js-cookie";
 import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import { Input } from "@mui/material";
-import TextareaAutosize from "@mui/base/TextareaAutosize";
+import * as yup from "yup";
+import { useFormik } from "formik";
 import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -67,40 +58,51 @@ BootstrapDialogTitle.propTypes = {
 };
 
 const EditDumpsterModal = (props) => {
-  React.useEffect(() => {
-    console.log(props.openModal);
-  }, [props.openModal]);
   const [coordinate, setCoordinate] = React.useState({
     latitude: 0,
     longitude: 0,
   });
-  const { register, handleSubmit, unregister } = useForm();
+  const [error, setError] = React.useState(null);
+  const dumpsterErrorHandling = yup.object().shape({
+    street: yup.string().required("Street is required"),
+    purok: yup.string().required("Purok is required"),
+    barangay: yup.string().required("Barangay is required"),
+    town: yup.string().required("Town is required"),
+    postal_code: yup.string().required("Postal Code is required"),
+  });
   React.useEffect(() => {
     setCoordinate({
       latitude: props.data[3],
       longitude: props.data[4],
     });
-    unregister("street");
-    unregister("purok");
-    unregister("barangay");
-    unregister("town");
-    unregister("postal_code");
+    values.street = props.data[1].split(", ")[0];
+    values.purok = props.data[1].split(", ")[1];
+    values.barangay = props.data[1].split(", ")[2];
+    values.town = props.data[1].split(", ")[3];
+    values.postal_code = props.data[2];
   }, [props.data[0], props.openModal]);
-  const onSubmit = (data) => {
-    if(data.street === props.data[1].split(", ")[0] && data.purok === props.data[1].split(", ")[1] && data.barangay === props.data[1].split(", ")[2] && data.town === props.data[1].split(", ")[3] && data.postal_code === props.data[2] && coordinate.latitude === props.data[3] && coordinate.longitude === props.data[4]){
+  const handleFormSubmit = (values, { resetForm }) => {
+    if (
+      values.street === props.data[1].split(", ")[0] &&
+      values.purok === props.data[1].split(", ")[1] &&
+      values.barangay === props.data[1].split(", ")[2] &&
+      values.town === props.data[1].split(", ")[3] &&
+      values.postal_code === props.data[2] &&
+      coordinate.latitude === props.data[3] &&
+      coordinate.longitude === props.data[4]
+    ) {
       props.setOpenModal(false);
-      
-    }else{
+    } else {
       if (coordinate.latitude != 0 && coordinate.longitude != 0) {
         axios
           .put(
             `http://localhost:8000/admin/dumpster/edit-dumpster/${props.data[0]}`,
             {
-              street: data.street,
-              purok: data.purok,
-              barangay: data.barangay,
-              town: data.town,
-              postal_code: data.postal_code,
+              street: values.street,
+              purok: values.purok,
+              barangay: values.barangay,
+              town: values.town,
+              postal_code: values.postal_code,
               latitude: coordinate.latitude,
               longitude: coordinate.longitude,
               accessToken: Cookies.get("user_id"),
@@ -108,15 +110,30 @@ const EditDumpsterModal = (props) => {
           )
           .then((res) => {
             if (res.data.success) {
+              resetForm();
               props.setOpenModal(false);
               props.setMesAlert(true);
               props.setMessage(res.data);
             }
           });
+      } else {
+        setError("Please select a designated location for the dumpster");
       }
     }
-   
   };
+  const { handleChange, handleSubmit, handleBlur, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        street: props.data[1].split(", ")[0],
+        purok: props.data[1].split(", ")[1],
+        barangay: props.data[1].split(", ")[2],
+        town: props.data[1].split(", ")[3],
+        postal_code: props.data[2],
+      },
+      enableReinitialize: true,
+      validationSchema: dumpsterErrorHandling,
+      onSubmit: handleFormSubmit,
+    });
   const handleClick = (map, event) => {
     setCoordinate({ latitude: event.lngLat.lat, longitude: event.lngLat.lng });
     console.log(event.lngLat);
@@ -134,136 +151,132 @@ const EditDumpsterModal = (props) => {
       >
         Edit Dumpster
       </BootstrapDialogTitle>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <DialogContent dividers>
-          <div style={{ height: "38vh", width: "100%" }}>
-            <Map
-              style="mapbox://styles/mapbox/streets-v9"
-              containerStyle={{
-                height: "36vh",
-                width: "100%",
-              }}
-              center={
-                coordinate.latitude == 0 && coordinate.longitude == 0
-                  ? [props.data[4], props.data[3]]
-                  : [coordinate.longitude, coordinate.latitude]
-              }
-              zoom={
-                (props.data[3] != 0 && props.data[4] != 0) ||
-                (coordinate.latitude != 0 && coordinate.longitude != 0)
-                  ? [15]
-                  : [11]
-              }
-              onClick={handleClick}
-            >
-              {(props.data[3] != 0 && props.data[4] != 0) ||
-              (coordinate.latitude != 0 && coordinate.longitude != 0) ? (
-                <Marker
-                  coordinates={
-                    coordinate.latitude == 0 && coordinate.longitude == 0
-                      ? [props.data[4], props.data[3]]
-                      : [coordinate.longitude, coordinate.latitude]
-                  }
-                  anchor="bottom"
-                >
-                  <img style={mystyle} src="/dumpster_marker_icon.png" />
-                </Marker>
-              ) : (
-                <></>
-              )}
-            </Map>
-          </div>
-          <Box sx={{ width: "100%" }}>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="street"
-              label="Street"
-              type="text"
-              fullWidth
-              defaultValue={props.data[1].split(", ")[0]}
-              required
-              {...register("street")}
-              variant="standard"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="purok"
-              label="Purok"
-              type="text"
-              fullWidth
-              defaultValue={props.data[1].split(", ")[1]}
-              required
-              {...register("purok")}
-              variant="standard"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="barangay"
-              label="Barangay"
-              type="text"
-              fullWidth
-              defaultValue={props.data[1].split(", ")[2]}
-              required
-              {...register("barangay")}
-              variant="standard"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="town"
-              label="Town"
-              type="text"
-              fullWidth
-              defaultValue={props.data[1].split(", ")[3]}
-              required
-              {...register("town")}
-              variant="standard"
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="postal"
-              label="Postal Code"
-              type="text"
-              fullWidth
-              defaultValue={props.data[2]}
-              required
-              {...register("postal_code")}
-              variant="standard"
-            />
-            {/* <TextField
-        autoFocus
-        margin="dense"
-        id="title"
-        label=  "Latitude"
-        value={coordinate.latitude == 0 ? props.data[3]:coordinate.latitude}
-        type="text"
-        fullWidth
-        variant="standard"
-        
-      />
-      <TextField
-        autoFocus
-        margin="dense"
-        id="title"
-        label=  "Longitude"
-        value={coordinate.longitude == 0 ? props.data[4]:coordinate.longitude}
-        type="text"
-        fullWidth
-        variant="standard"
-        
-      /> */}
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <button type="submit" className="btn btn-success">
-            Edit
-          </button>
-        </DialogActions>
-      </form>
+      <DialogContent dividers>
+        <div style={{ height: "38vh", width: "100%" }}>
+          <Map
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+              height: "36vh",
+              width: "100%",
+            }}
+            center={
+              coordinate.latitude == 0 && coordinate.longitude == 0
+                ? [props.data[4], props.data[3]]
+                : [coordinate.longitude, coordinate.latitude]
+            }
+            zoom={
+              (props.data[3] != 0 && props.data[4] != 0) ||
+              (coordinate.latitude != 0 && coordinate.longitude != 0)
+                ? [15]
+                : [11]
+            }
+            onClick={handleClick}
+          >
+            {(props.data[3] != 0 && props.data[4] != 0) ||
+            (coordinate.latitude != 0 && coordinate.longitude != 0) ? (
+              <Marker
+                coordinates={
+                  coordinate.latitude == 0 && coordinate.longitude == 0
+                    ? [props.data[4], props.data[3]]
+                    : [coordinate.longitude, coordinate.latitude]
+                }
+                anchor="bottom"
+              >
+                <img style={mystyle} src="/dumpster_marker_icon.png" />
+              </Marker>
+            ) : (
+              <></>
+            )}
+          </Map>
+          {error && <p className="text-danger small text-center">{error}</p>}
+        </div>
+        <Box sx={{ width: "100%" }}>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="street"
+            label="Street"
+            type="text"
+            fullWidth
+            value={values.street}
+            onChange={handleChange("street")}
+            onBlur={handleBlur("street")}
+            variant="standard"
+          />
+          {errors.street && touched.street && (
+            <p className="text-danger small ">{errors.street}</p>
+          )}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="purok"
+            label="Purok"
+            type="text"
+            fullWidth
+            value={values.purok}
+            onChange={handleChange("purok")}
+            onBlur={handleBlur("purok")}
+            variant="standard"
+          />
+          {errors.purok && touched.purok && (
+            <p className="text-danger small ">{errors.purok}</p>
+          )}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="barangay"
+            label="Barangay"
+            type="text"
+            fullWidth
+            value={values.barangay}
+            onChange={handleChange("barangay")}
+            onBlur={handleBlur("barangay")}
+            variant="standard"
+          />
+          {errors.barangay && touched.barangay && (
+            <p className="text-danger small ">{errors.barangay}</p>
+          )}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="town"
+            label="Town"
+            type="text"
+            fullWidth
+            value={values.town}
+            onChange={handleChange("town")}
+            onBlur={handleBlur("town")}
+            variant="standard"
+          />
+          {errors.town && touched.town && (
+            <p className="text-danger small ">{errors.town}</p>
+          )}
+          <TextField
+            autoFocus
+            margin="dense"
+            id="postal"
+            label="Postal Code"
+            type="text"
+            fullWidth
+            value={values.postal_code}
+            onChange={handleChange("postal_code")}
+            onBlur={handleBlur("postal_code")}
+            variant="standard"
+          />
+          {errors.postal_code && touched.postal_code && (
+            <p className="text-danger small ">{errors.postal_code}</p>
+          )}
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <button
+          type="submit"
+          className="btn btn-success"
+          onClick={handleSubmit}
+        >
+          Edit
+        </button>
+      </DialogActions>
     </Dialog>
   );
 };

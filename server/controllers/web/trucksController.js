317de50d0025350c7f9787b7cfe0f38750fd8a87
Brant <uy.brantfrancis@gmail.com>
2022-02-24 16:50:58 +0800

    Truck functions completed except get functions

--- server/controllers/web/trucksController.js
+++ server/controllers/web/trucksController.js
@@ -14,6 +14,11 @@ const generateAccessToken = (user) =>{
 }
 // Start of Truck Model Controllers
 
+function mockLogin(params) {
+    
+}
+
+
 exports.addTruck = async(req, res) => {
 
 
@@ -33,13 +38,22 @@ exports.addTruck = async(req, res) => {
 
     if(req.body.accessToken){
         jwt.verify(req.body.accessToken,process.env.ACCESS_TOKEN_SECRET, async(err, decoded) => {
+
             var admin = await user.model.findAll({
                 where:{
                     user_id:decoded.user_id,
                     user_type:"Admin"
                 }
             })
-            if (admin) {
+
+            var driver = await user.model.findAll({
+                where:{
+                    fname:req.body.driverName,
+                    user_type:"Driver"
+                }
+            })
+            if (admin && driver) {
+
                 let truckExist = await truck.model.findAll({
                     where:{
                         plate_no:req.body.plate_no
@@ -52,7 +66,14 @@ exports.addTruck = async(req, res) => {
                         model:req.body.model
                     })
                     if (truck) {
-                        return res.send("Truck Record Successfully Created");
+
+                        let truckAssignment = await truck_assignment.model.create({
+                            truck_id: truck.truck_id,
+                            driver_id: driver.user_id,
+                        })
+                        if (truckAssignment) {
+                            return res.send("Truck Record and Assignment Successfully Created");
+                        }
                     } else {
                         return res.send("something went wrong the truck record wasn't created");
                     }
@@ -94,29 +115,48 @@ exports.updateTruck = async(req, res) => {
                 truck_id: req.params.id
             }
         }
-
-       
     )
+    if (truck) {
+        return res.send("Truck Record Successfully updated");
+    } else {
+        return res.send("Something went balistic");
+    }
+  
+}
 
-    let data = await task.model.update(
-        {taskName : req.body.taskName,
-         description: req.body.description,
-         dueDate : req.body.dueDate
-        },
-
+exports.deactivateTruck = async(req,res) => {  //move to maintenance
+    let truck = await truck.model.update(
         {
-            where: {
-                id: req.params.id
+            active:0
+        },{
+            where:{
+                truck_id:req.params.id
             }
         }
     )
-    
+    if (truck) {
+        return res.send("Truck has now been moved to UNDER MAINTENANCE");
+    } else {
+        return res.send("Something went wrong");
+    }
 }
 
-exports.maintenaceTruck = async(req,res) => {
-
+exports.activateTruck = async(req,res) => {     //move to active trucks
+    let truck = await truck.model.update(
+        {
+            active:1
+        },{
+            where:{
+                truck_id:req.params.id
+            }
+        }
+    )
+    if (truck) {
+        return res.send("Truck has now been moved to UNDER MAINTENANCE");
+    } else {
+        return res.send("Something went wrong");
+    }
 }
-
 
 exports.displayTruck = async(req, res) => {         //specific truck record
 
@@ -127,6 +167,8 @@ exports.displayTrucks = async(req, res) => {
 }
 
 // End of Truck model controllers
+
+
 
 
 
@@ -189,5 +231,45 @@ exports.displayTrucks = async(req, res) => {
 //                 return res.send("Truck already exists");
 //             }
 //         })
+//     }
+// }
+
+
+
+// exports.deleteUser = async(req,res) => {
+//     let data = user.model.destroy({
+//         where:{
+//             user_id: req.params.id //Id of the user I wanna delete will be passed (this id should be stored in the delete button) 
+//         }
+//     }).then(function(rowDeleted){
+//         if(rowDeleted===1){
+//             res.send({value:data}); 
+//             console.log('Record Deleted Successfully');
+//         }
+//     }, function(err){
+//         console.log(err);
+//     })
+// }
+
+
+// //Need to test this first
+// exports.updateUser = async(req,res) => {
+//     //add the needed data to do the updates
+//     const updateObject = req.body
+//     //Medj big bug ni siya kay if mag send ka part sa object og new password ma change siya so if naa password apil sa req 
+//     //matic error
+//     if (req.body.password) {
+//         return res.status(500).send('something went wrong');
+//     }
+
+//     let data = await user.model.update(updateObject, {
+//         where:{
+//             user_id: req.params.id
+//         }
+//     })
+    
+//     if (data) {
+//         res.send({value:data}); 
+//         console.log('record has been updated');
 //     }
 // }
